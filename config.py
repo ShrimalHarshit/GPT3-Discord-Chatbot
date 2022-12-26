2f0461c95d89f8bca4021a1bfcd6ee894288a4b4
ShrimalHarshit <106028768+ShrimalHarshit@users.noreply.github.com>
2022-12-26 23:47:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = "discord_bot_token"
+token = "MTA1Njk0MTc2OTAzNzcxNzU0NA.GFo4SL.Kl_2eK-Ge3_cggDvt0teCIU5ck6HAEO4mhK5v0"
 status = "remind me to fix bugs"
 desc = "This is chat between Human and Bot. Bot is an AI assistant and knows almost everything. It is designed to assist and help humans and provide them detailed answers to any questions that they ask.\n"
